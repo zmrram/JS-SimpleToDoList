3c53c232ee8c6eddbb3f5993727408a81b1468d1
Tin Huynh <Tin Huynh>
2017-04-20 20:53:31 -0700

    add checkbox and delete task function, however if the 2 task next to each other are selected then only 1 will get delete

--- js/todo.js
+++ js/todo.js
@@ -3,18 +3,40 @@
 var todoList = [];
 
 function insertItem(value) {
+	todoList.push(value);
 	var theList = document.getElementById('thelist');
-	thelist.innerHTML += "<li>" + value + "</li>";
+	var list = document.createElement("li");
+	var check = document.createElement("INPUT");
+	check.type = "checkbox";
+	check.name = 'checkeditem';
+	list.innerHTML =  value ;
+	thelist.appendChild(list);
+	list.appendChild(check);
+	console.log(todoList.length);
+}
+
+function deleteCheckedItem(){
+	var listArr = document.getElementById('thelist').children;
+	for (var i = 0; i < listArr.length; i++){
+		if (listArr[i].children[0].checked){
+			listArr[i].remove();
+		}
+	}
 }
 
 function listener() {
 	var insert = document.getElementById('insertList').elements['addButton'];
+	var itemClear = document.getElementById('deleteButton');
 	insert.onclick = function (e){
 		e.preventDefault();
 		var todo_item = document.getElementById('insertList').elements['userInput'].value;
 		if (todo_item != null){
 			insertItem(todo_item);
 		}
+	};
+	itemClear.onclick = function (e) {
+		e.preventDefault();
+		deleteCheckedItem();
 	};
 }
 
